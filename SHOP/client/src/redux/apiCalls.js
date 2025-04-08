6d5f13783d42c2029f62fa3d36cc0c8252785f99
SimBisha <ramaerjon44@gmail.com>
2025-04-08 20:41:10 +0200

    Update apiCalls.js

--- SHOP/client/src/redux/apiCalls.js
+++ SHOP/client/src/redux/apiCalls.js
@@ -1,11 +1,11 @@
 import { loginStart } from "./userRedux"
 import { loginSuccess, loginFailure } from "./userRedux"
-import axios from "axios"
+import { publicRequest } from "../requestMethods"
 
 export const login = async (dispatch, user) => {
     dispatch(loginStart())
     try {
-        const res = await axios.post("http://localhost:5000/api/auth/login", user);
+        const res = await publicRequest.post("http://localhost:5000/api/auth/login", user);
         dispatch(loginSuccess(res.data));
     } catch (err) {
         dispatch(loginFailure())
